43a7700bb42922815f8d0b33ffeb735db2402c66
Andrea Massignani <feelsgreat2016@gmail.com>
2017-06-20 03:03:53 +0000

    add keypad key divs, add basic grid layout

--- main.js
+++ main.js
@@ -2,5 +2,5 @@ document.addEventListener('DOMContentLoaded', function(evt) {
     var h = document.getElementById('calculator-name')
     console.log(h)
     
-    h.style.color='yellow'
+    // h.style.color='yellow'
 })
